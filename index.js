e3b82c0850c1f70e2b2bd6b04cdc64e9922f5620
hishanky <developer.ankitshahu@gmail.com>
2021-12-29 14:04:57 +0530

    sample api done

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/products", (req, res) => {
+  res.send([
+    {
+      Username: "Ankit shahu",
+      password: "shanky",
+    },
+    {
+      Username: "Hello world",
+      password: "shanky",
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log("Express app is listening on port no " + port);
+});
